29cf08e88ee72dc07fc33d6f5dfa8599857d5015
Jihane FATHI <86119879+Jihane12@users.noreply.github.com>
2026-06-29 19:59:49 +0200

    Update variables.tf

---
+++ infra/variables.tf
@@ -0,0 +1,4 @@
+variable "app_name"      { type = string }
+variable "acr_registry"  { type = string }
+variable "acr_username"  { type = string }
+variable "acr_password"  { type = string sensitive = true }
